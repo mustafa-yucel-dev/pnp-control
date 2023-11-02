f1df6875308a06337e473407fc14ca1e3eb55b38
Mustafa Yucel <muus.yucel@gmail.com>
2023-11-02 18:38:29 -0400

    more pictures for the documentation

---
+++ Marseille/arroyo_5240.py
@@ -0,0 +1,63 @@
+import serial
+
+class Arroyo5240(object):
+    def __init__(self, port='COM4'):
+        self.ser = serial.Serial(port)
+        self.ser.baudrate = 38400
+        self.ser.bytesize = serial.EIGHTBITS
+        self.ser.parity = serial.PARITY_NONE
+        self.ser.stopbits = serial.STOPBITS_ONE
+
+    def send_rcv(self, command):
+        self.ser.write((command + '\n').encode())
+        answer = self.ser.readline()
+        return answer.strip().decode()
+
+    def send(self, command):
+        self.ser.write((command + '\n').encode())
+
+    def reset(self):
+        self.ser.write(b'*RST\n')
+
+    def decrease_temp(self, steps):
+        self.ser.write(('TEC:DEC ' + str(steps) + '\n').encode())
+
+    def increase_temp(self, steps):
+        command = 'TEC:INC ' + str(steps) + '\n'
+        self.ser.write(command.encode())
+
+    def get_temp(self):
+        self.ser.write(b'TEC:T?\n')
+        temp = self.ser.readline()
+        return float(temp.strip())
+
+    def set_temp(self, temp):
+        self.ser.write(('TEC:T ' + str(temp) + '\n').encode())
+
+    def get_set_temp(self):
+        self.ser.write(b'TEC:T?\n')
+        set_temp = self.ser.readline()
+        return float(set_temp.strip())
+
+    def get_step(self):
+        self.ser.write(b'TEC:TSTEP?\n')
+        step = self.ser.readline()
+        return float(step.strip())
+
+    def set_step(self, step):
+        self.ser.write(('TEC:TSTEP ' + str(step) + '\n').encode())
+
+    def set_temp_limits(self, t_hi, t_lo):
+        self.ser.write(('TEC:LIM:THI ' + str(t_hi) + '\n').encode())
+        self.ser.write(('TEC:LIM:TLO ' + str(t_lo) + '\n').encode())
+
+    def autotune(self, test_temp):
+        self.ser.write(('TEC:AUTOTUNE ' + str(test_temp) + '\n').encode())
+
+
+if __name__ == '__main__':
+    import time
+
+    obj = Arroyo5240()
+    print(obj.get_temp())
+    obj.set_temp(20)
--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Documentation for Programmable NanoPhotonics Processor
-
-This repository has codes with plenty of files. The files are used to program the instruments that monitors and control the PNP. There are also function that are used for a better data treatement by using the instruments functions.
+![Entire setup](top_lvl_view.jpg)
+This repository has codes with plenty of files. The files are used to program the instruments that monitors and control the PNP. There are also function that are used for a better data treatement by using the instruments functions. 
 ![Architecture of a 4x4 PNP](4x4_pnp.png)
 ![Example output of the 4x4 PNP](output.png)
 # Devices
@@ -14,6 +14,8 @@ The HMP4040 is a good system that is used as a power supply for most of the syst
 ## Arroyo 5240
 The Arroyo 5240 is a device allowing to control the temperature with a feedback loop. In this case, I use the Peltier element (TECD2) amd the thermistor (TH10K) from Thorlabs.
 ## NiDAQ6343 
-The NIDAQ 6343 has the advantage of being programmable with the usual DAQ MX software that interfaces with Python. The NiDAQ6343 has an aggregate speed of 500kHz reading speed with 32 Analog input (16 being digital).
+The NIDAQ 6343 has the advantage of being programmable with the usual DAQ MX software that interfaces with Python. The NiDAQ6343 has an aggregate speed of 500kHz reading speed with 32 Analog input (16 being digital). This is used to read out the custom photodetector array. The PhDs are from Coheron company in France.
+![NiDAQ and PhDs array](pd_array.jpg)
 # PNP Control
 This jupyter notebook uses some function of the function file to characterize the chip by tunning the devices on the PNP. The function file is well documented.
+![Chip and fibre array](chip.jpg)
